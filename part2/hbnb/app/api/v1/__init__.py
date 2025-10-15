c95d15e8eebcc5f4a67d8aa624fefd8787f719e0
jumpcc693 <tvop1218@gmail.com>
2025-10-15 10:04:13 +0200

    feat: implement review management API with CRUD operations and models

--- part2/hbnb/app/api/v1/__init__.py
+++ part2/hbnb/app/api/v1/__init__.py
@@ -1,4 +1,18 @@
-# TODO: Initialize API v1 module
-# TODO: Import all endpoint modules (users, places, reviews, amenities)
-# TODO: Register all namespaces with the API
-# TODO: Set up v1-specific configurations
+from flask_restx import Api
+from flask import Blueprint
+from .users import api as users_ns
+from .places import api as places_ns
+from .reviews import api as reviews_ns
+
+blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+
+api = Api(
+    blueprint,
+    title='HBnB REST API',
+    version='1.0',
+    description='API for HBnB project (users, places, reviews)'
+)
+
+api.add_namespace(users_ns)
+api.add_namespace(places_ns)
+api.add_namespace(reviews_ns)
