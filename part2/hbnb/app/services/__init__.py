81d6135903813fec814eb715bba3680116bede7c
jumpcc693 <tvop1218@gmail.com>
2025-10-16 15:11:02 +0200

    feat: implement initial Flask application with a simple hello world endpoint

--- part2/hbnb/app/services/__init__.py
+++ part2/hbnb/app/services/__init__.py
@@ -1,12 +1,4 @@
-from flask import Flask
-from flask_restx import Api
-from app.api.v1.reviews import api as reviews_ns
+from .facade import HBnBFacade
 
-def create_app():
-    app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API')
-
-    # Enregistrer le namespace
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-
-    return app
+# Create a singleton instance of the facade
+facade = HBnBFacade()
