2d1625f209b089b6b84aa9aa25af7a3c5580cb4b
NikkoAvardes <11460@holbertonstudents.com>
2025-11-22 13:25:18 +0100

    changement

--- part3/hbnb/sql_scripts/insert_data.sql
+++ part3/hbnb/sql_scripts/insert_data.sql
@@ -7,4 +7,4 @@ VALUES ("36c9050e-ddd3-4c3b-9731-9f487208bbc1", "admin@hbnb.io", "Admin", "HBnB"
 INSERT INTO amenities (id, name) VALUES 
 ("df0a11be-f30d-4a85-b532-64f420d3ddca", "WiFi"),
 ("1ec9c5ce-5317-4a99-b4ab-c92842f252a7", "Swimming Pool"),
-("7282922e-fb89-4219-ac23-714b8254b085", "Air Conditioning");Login failed: Load failed
+("7282922e-fb89-4219-ac23-714b8254b085", "Air Conditioning");
